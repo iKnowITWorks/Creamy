91bc8f06cd6a3a2bf758f4074894283fbb576c38
Chris L <chris@iknowitworks.org>
2017-04-20 17:46:05 +0800

    First commit.

---
+++ php/Config.php
@@ -0,0 +1,13 @@
+<?php
+// specific instalation configuration
+
+// database configuration
+define('DB_USERNAME', 'creamcrm');
+define('DB_PASSWORD', 'creamcrm');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'db_creamcrm');
+define('DB_PORT', '3306');
+
+// other configuration parameters			
+define('CRM_ADMIN_EMAIL', 'admin@iknowitworks.org');
+?>
---
+++ php/CreateCustomer.php
@@ -0,0 +1,170 @@
+<?php
+/**
+	The MIT License (MIT)
+	
+	Copyright (c) 2015 Ignacio Nieto Carvajal
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+require_once('DbHandler.php');
+require_once('CRMDefaults.php');
+require_once('LanguageHandler.php');
+require('Session.php');
+
+$lh = \creamy\LanguageHandler::getInstance();
+$user = \creamy\CreamyUser::currentUser();
+
+// check required fields
+$validated = 1;
+if (!isset($_POST["name"])) {
+	$validated = 0;
+}
+if (!isset($_POST["customer_type"])) {
+	$validated = 0;
+}
+
+if ($validated == 1) {
+	$db = new \creamy\DbHandler();
+
+	// get name (mandatory)
+	$name = $_POST["name"];
+	$name = stripslashes($name);
+	$name = $db->escape_string($name);
+	$customerType = $_POST["customer_type"];
+	$customerType = stripslashes($customerType);
+	$customerType = $db->escape_string($customerType);
+	$createdByUser = $user->getUserId();
+		
+	// get optional values
+	
+	// email
+	$email = NULL; if (isset($_POST["email"])) { 
+		$email = $_POST["email"]; 
+		$email = stripslashes($email);
+		$email = $db->escape_string($email);
+	}
+	// phone
+	$phone = NULL; if (isset($_POST["phone"])) { 
+		$phone = $_POST["phone"];
+		$phone = stripslashes($phone);
+		$phone = $db->escape_string($phone); 
+	}
+	// mobile phone
+	$mobile = NULL; if (isset($_POST["mobile"])) { 
+		$mobile = $_POST["mobile"];
+		$mobile = stripslashes($mobile);
+		$mobile = $db->escape_string($mobile); 
+	}
+	// ID number (passport, DNI, NIF, identification number, VAT number, etc...).
+	$id_number = NULL; if (isset($_POST["id_number"])) { 
+		$id_number = $_POST["id_number"]; 
+		$id_number = stripslashes($id_number);
+		$id_number = $db->escape_string($id_number);
+	} 
+	// address
+	$address = NULL; if (isset($_POST["address"])) { 
+		$address = $_POST["address"]; 
+		$address = stripslashes($address);
+		$address = $db->escape_string($address);
+	}
+	
+	// city
+	$city = NULL; if (isset($_POST["city"])) { 
+		$city = $_POST["city"]; 
+		$city = stripslashes($city);
+		$city = $db->escape_string($city);
+	}
+	
+	// state
+	$state = NULL; if (isset($_POST["state"])) { 
+		$state = $_POST["state"]; 
+		$state = stripslashes($state);
+		$state = $db->escape_string($state);
+	}
+	
+	// ZIP code
+	$zipcode = NULL; if (isset($_POST["zipcode"])) { 
+		$zipcode = $_POST["zipcode"]; 
+		$zipcode = stripslashes($zipcode);
+		$zipcode = $db->escape_string($zipcode);
+	}
+	
+	// country
+	$country = NULL; if (isset($_POST["country"])) { 
+		$country = $_POST["country"]; 
+		$country = stripslashes($country);
+		$country = $db->escape_string($country);
+	}
+	
+	// website
+	$website = NULL; if (isset($_POST["website"])) { 
+		$website = $_POST["website"]; 
+		$website = stripslashes($website);
+		$website = $db->escape_string($website);
+	}	
+			
+	// notes
+	$notes = NULL; if (isset($_POST["notes"])) { 
+		$notes = $_POST["notes"]; 
+		$notes = stripslashes($notes);
+		$notes = $db->escape_string($notes);
+	}
+	
+	// fecha de nacimiento
+	$birthdate = NULL; if (isset($_POST["birthdate"])) { 
+		$birthdate = $_POST["birthdate"]; 
+		$birthdate = stripslashes($birthdate);
+		$birthdate = $db->escape_string($birthdate);
+	}
+
+	// marital status
+	$maritalstatus = 0; if (isset($_POST["maritalstatus"])) { 
+		$maritalstatus = $_POST["maritalstatus"]; 
+		$maritalstatus = stripslashes($maritalstatus);
+		$maritalstatus = $db->escape_string($maritalstatus);
+	}
+	if ($maritalstatus < 1 || $maritalstatus > 5) $maritalstatus = 0;
+		
+	// gender
+	$gender = NULL; if (isset($_POST["gender"])) { 
+		$gender = $_POST["gender"]; 
+		$gender = stripslashes($gender);
+		$gender = $db->escape_string($gender);
+	}
+	if ($gender < 0 || $gender > 1) $gender = NULL;
+	
+	// product type
+	$productType = NULL; if (isset($_POST["productType"])) { 
+		$productType = $_POST["productType"]; 
+		$productType = stripslashes($productType);
+		$productType = $db->escape_string($productType);
+	}
+	
+	// do not send email
+	$donotsendemail = 0; if (isset($_POST["donotsendemail"])) { 
+		$donotsendemail = 1;
+	}
+
+	// create customer and return result.
+	$result = $db->createCustomer($customerType, $name, $email, $phone, $mobile, $id_number, $address, $city, $state, $zipcode, $country, $birthdate, $maritalstatus, $productType, $donotsendemail, $createdByUser, $gender, $notes, $website);
+	if ($result === true) { ob_clean(); print CRM_DEFAULT_SUCCESS_RESPONSE; }
+	else { ob_clean(); $lh->translateText("unable_create_customer"); } 
+} else { ob_clean(); $lh->translateText("some_fields_missing"); }
+?>
